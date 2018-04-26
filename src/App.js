71a920af531a2ad992146221da1f2697a2322c04
kapillikes@gmail.com <nk3527@gmail.com>
2018-04-26 19:45:39 +0530

    path rearranged

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from 'react';
 import { Router, browserHistory, Route, Link} from 'react-router';
-import logo from './logo.svg';
-import './App.css';
 
-const Page = ({ title }) => (
-    <div className="App">
-        <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h2>{title}</h2>
-        </div>
-        <p className="App-intro">
-            This is the {title} page.
-        </p>
-        <p>
-            <Link to="/">Home</Link>
-        </p>
-        <p>
-            <Link to="/about">About</Link>
-        </p>
-        <p>
-            <Link to="/settings">Settingsssss</Link>
-        </p>
-    </div>
-);
+import './App.css';
+import Page from './pages/Page';
 
 const Home = (props) => (
-    <Page title="Home"/>
+    <Page title="Home" />
 );
 
 const About = (props) => (
