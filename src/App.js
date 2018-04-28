0397155fc34f778909c8cf03bdf15238167d55e9
kapillikes@gmail.com <nk3527@gmail.com>
2018-04-28 20:31:00 +0530

    warning resolved

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import { Router, browserHistory, Route} from 'react-router';
-
 import './App.css';
-import Page from './pages/Page';
 import Main from './pages/Main';
 
 const Home = (props) => (
     <Main title="Home" />
 );
 
-const About = (props) => (
-    <Page title="About"/>
-);
-
-const Settings = (props) => (
-    <Page title="Settings"/>
-);
 
 class App extends Component {
     render() {
